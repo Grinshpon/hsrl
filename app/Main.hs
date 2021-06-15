93c41da857c7f0c3517342cd05b63fa98bd9eee3
grinshpon <grinshpondaniel@protonmail.com>
2021-06-15 00:38:09 -0400

    graphics work

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,10 @@ import Grid
 
 import Pine
 import qualified SDL
+import Foreign.C.Types (CInt)
+
+import Data.Vector (Vector)
+import qualified Data.Vector as V
 
 data MyGrid = MyGrid [[Int]]
 
@@ -36,18 +40,42 @@ myGrid = MyGrid
   ,[0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ,[0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-data MyState = Logo Image
+data RLState = RLState
+  { logo :: Image
+  , tileset :: Vector Image
+  , consoleResolution :: (Int, Int) --should be CInt?
+  , tileResolution :: (Int, Int)
+  }
 
-defaultInitial = Logo $ newImage "src/Media/logo.png" Nothing (Just $ rect 0 0 400 400)
+defaultInitial = RLState
+  (newImage "src/Media/logo.png" Nothing (Just $ rect 0 0 400 400))
+  (tilesFromImg "src/Media/ascii.png" 10 10 10)
+  (100, 75)
+  (10, 10)
 
-instance Stateful MyState where
+tilesFromImg :: FilePath -> CInt -> CInt -> CInt -> Vector Image
+tilesFromImg fp w h scale =
+  V.fromList
+    [newImage fp (Just $ rect (x*w) (y*h) w h) dim | y <- [0..15], x <- [0..15]]
+  where
+    dim = Just $ rect 0 0 (w*scale) (h*scale)
+
+instance Stateful RLState where
   update _ Load                      = contLog "Hello, Pine!"
   update _ WindowClose               = quit
   update _ (KeyPressed SDL.KeycodeQ) = quitLog "Goodbye, Pine!"
   update _ _                         = cont
 
-instance Drawable MyState where
-  draw (Logo img) = translated 200 200 $ fromImage img
+instance Drawable RLState where
+  draw rl = fromImage $ (tileset rl) V.! 1 --translated 200 200 $ fromImage $ logo rl
+
+myConfig = withDefaultConfig
+  { SDL.windowHighDPI = True
+  , SDL.windowInitialSize = SDL.V2 1000 750
+  }
+
+-- brogue is about 100x34 "tiles" (manually counted)
+-- I'm thinking of doing 100x75 square tiles (4:3)
 
 main :: IO ()
 main = do
@@ -66,4 +94,4 @@ main = do
   --printPath myGrid (0,0) (12,3)
   --printPath myGrid (5,5) (12,3)
   --printPath myGrid (0,0) (1,15)
-  pine "Hello Pine" withDefaultConfig defaultInitial
+  pine "Hello Pine" myConfig defaultInitial
